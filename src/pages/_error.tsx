ffaf676b788418896e29b4044fb5d4ade77ea721
Anton Kulyk <kuliks.anton@gmail.com>
2020-04-27 14:57:32 +0300

    Add custom error page

---
+++ src/pages/_error.tsx
@@ -0,0 +1,51 @@
+import {Emoji, Layout} from '../components';
+
+function Error(): React.ReactElement {
+  return (
+    <>
+      <Layout title="Error">
+        <div id="error-container">
+          <p id="emoji">
+            <Emoji name="sad">😬</Emoji>
+          </p>
+          <h1>Oops, this is embarrassing</h1>
+          <p className="message first-message">
+            Sorry, looks like something went wrong with the website
+          </p>
+          <p className="message">
+            I had to get an alert, so it will be up ASAP
+          </p>
+        </div>
+      </Layout>
+      <style jsx>{`
+        #emoji {
+          font-size: 3rem;
+        }
+
+        #error-container {
+          margin: auto;
+          display: flex;
+          flex: 1;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        h1 {
+          text-align: center;
+        }
+
+        .message {
+          text-align: center;
+          padding: 3px 0;
+        }
+
+        .first-message {
+          margin-top: 20px;
+        }
+      `}</style>
+    </>
+  );
+}
+
+export default Error;
