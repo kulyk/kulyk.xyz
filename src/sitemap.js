8c59091342e0c63541f3cc24316cc7d329d6f9ee
Anton Kulyk <kuliks.anton@gmail.com>
2020-04-30 16:34:23 +0300

    Fix sitemap generation

--- src/sitemap.js
+++ src/sitemap.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const sitemap = require('nextjs-sitemap-generator');
 
-const ASSETS_PATH = path.join(__dirname, '../public');
+const DEST = path.join(__dirname, '../out');
 
 sitemap({
   baseUrl: 'https://kulyk.xyz',
   ignoredExtensions: ['png', 'jpg'],
   ignoredPaths: ['404'],
-  pagesDirectory: path.join(__dirname, '../out'),
-  targetDirectory: ASSETS_PATH,
+  pagesDirectory: DEST,
+  targetDirectory: DEST,
   nextConfigPath: path.join(__dirname, '../next.config.js'),
 });
