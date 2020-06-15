a3927cc6aaad0e4c6e25bcd9ebd108e991f0d7eb
Anton Kulyk <kuliks.anton@gmail.com>
2020-06-15 22:14:35 +0300

    Handle emojis on non Apple devices

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,3 +1,5 @@
+import EmojiRender, {Emojione} from 'react-emoji-render';
+
 type Props = {
   id?: string;
   name: string;
@@ -6,9 +8,12 @@ type Props = {
 
 function Emoji(props: Props): React.ReactElement {
   const {id, name, children} = props;
+  const isAppleDevice =
+    /iPad|iPhone|iPod|Mac/.test(navigator.userAgent) && !window.MSStream;
+  const Render = isAppleDevice ? EmojiRender : Emojione;
   return (
     <span id={id} role="img" aria-label={`${name} emoji`}>
-      {children}
+      <Render text={children} />
     </span>
   );
 }
