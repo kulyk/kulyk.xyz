d7acbfea9f818152bc0a58dbd4b4391b233829d6
Anton Kulyk <kuliks.anton@gmail.com>
2021-02-03 02:25:52 +0200

    Render MDX for RSS

--- src/scripts/rss.tsx
+++ src/scripts/rss.tsx
@@ -3,6 +3,7 @@ import path from 'path';
 import {Feed} from 'feed';
 import Config from '../config';
 import {PostCollectionScripted} from '../posts';
+import {renderToString} from '../mdx/render-to-string';
 import {getPostFullUrl} from '../utils';
 import {DEST} from './common';
 
@@ -35,17 +36,19 @@ async function generate(): Promise<void> {
   const posts = await collection.getAllPostsWithContent();
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  posts.forEach(({post, content}) => {
+  posts.forEach(async ({post, content}) => {
+    const formattedContent = await renderToString(content, {scope: post});
     feed.addItem({
       title: post.title,
       id: post.slug,
       link: getPostFullUrl(post.slug),
       description: post.description,
-      // content: ReactDOM.renderToStaticMarkup(<Markdown content={content} />),
+      content: formattedContent.renderedOutput,
       author: [author],
       date: new Date(post.publishedAt),
     });
   });
+
   fs.writeFileSync(path.join(DEST, 'feed.xml'), feed.rss2());
 }
 
