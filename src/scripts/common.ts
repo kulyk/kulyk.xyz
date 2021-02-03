881275926387d9bc3083137a3796e1adb517ad5f
Anton Kulyk <kuliks.anton@gmail.com>
2021-02-03 02:55:43 +0200

    Fix sitemap and RSS generation

--- src/scripts/common.ts
+++ src/scripts/common.ts
@@ -1,4 +1,4 @@
 import path from 'path';
 
 export const BASE_DIR = path.join(__dirname, '../..');
-export const DEST = path.join(BASE_DIR, 'out');
+export const DEST = path.join(BASE_DIR, 'public');
