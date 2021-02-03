fea49a95e64860b544cbe566d177701bc52bf329
Anton Kulyk <kuliks.anton@gmail.com>
2021-02-03 02:10:36 +0200

    Update favicons

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -15,35 +15,10 @@ function Page(props: PageProps): React.ReactElement {
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preload" as="font" type="font/ttf" href="/fonts/Inter.ttf" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicons/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicons/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <link rel="shortcut icon" href="/favicons/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#2b5797" />
-        <meta
-          name="msapplication-config"
-          content="/favicons/browserconfig.xml"
-        />
+        <link rel="icon" href="favicons/favicon.ico" />
+        <link rel="icon" href="favicons/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="favicons/apple-touch-icon.png" />
+        <link rel="manifest" href="favicons/site.webmanifest" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
       {children}
