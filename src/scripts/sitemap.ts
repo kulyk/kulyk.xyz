56137280932e59f98c1066066df9cb62c363e52d
Anton Kulyk <kuliks.anton@gmail.com>
2021-02-03 15:02:05 +0200

    Fix sitemap generation

--- src/scripts/sitemap.ts
+++ src/scripts/sitemap.ts
@@ -5,9 +5,23 @@ import {BASE_DIR, DEST} from './common';
 
 sitemap({
   baseUrl: Config.SITE_URL,
-  ignoredExtensions: ['png', 'jpg', 'txt'],
-  ignoredPaths: ['404'],
-  pagesDirectory: DEST,
+  ignoredExtensions: [
+    'js',
+    'map',
+    'png',
+    'jpg',
+    'jpeg',
+    'svg',
+    'ico',
+    'mp3',
+    'txt',
+    'xml',
+    'json',
+    'ttf',
+    'webmanifest',
+  ],
+  ignoredPaths: ['index', '404'],
+  pagesDirectory: path.join(BASE_DIR, '/.next/server/pages'),
   targetDirectory: DEST,
   nextConfigPath: path.join(BASE_DIR, 'next.config.js'),
 });
