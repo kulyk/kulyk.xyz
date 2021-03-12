88050751671fae24d070de0043e9b8a3fae31ee0
Anton Kulyk <kuliks.anton@gmail.com>
2021-03-13 00:18:29 +0200

    Add shadow to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,6 +12,8 @@ export function Card({className, ...props}: CardProps): React.ReactElement {
         padding: 16px 24px;
         background-color: ${theme.card.background};
         border-radius: 16px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+          0 6px 20px 0 rgba(0, 0, 0, 0.19);
       `}</style>
     </>
   );
